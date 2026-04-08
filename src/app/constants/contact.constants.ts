4dcda7a62cf0996d5e09b4c94fa1938f31ebd3bd
dhanushgowda89516 <dhanushgowda7444@gmail.com>
2026-04-08 11:36:26 +0530

    added favicon, updated contacts

--- src/app/constants/contact.constants.ts
+++ src/app/constants/contact.constants.ts
@@ -1,10 +1,10 @@
 export const CONTACT_INFO = {
-  address: 'Industrial Area, Sector 62, Noida, UP 201301',
-  phone: '+91 9876543210',
-  emergencyPhone: '+91 99999 88888',
+  address: '2nd Cross, Shankarpura, Chamarajanagar, Karnataka-571313',
+  phone: '+91 89516-41523',
+  emergencyPhone: '+91 82771-75828',
   emails: {
     info: 'info@industrialepc.com',
     support: 'support@industrialepc.com'
   },
-  businessHours: 'Mon - Fri, 9:00 AM to 6:00 PM'
+  businessHours: 'Mon - Sat, 9:00 AM to 6:00 PM'
 };
